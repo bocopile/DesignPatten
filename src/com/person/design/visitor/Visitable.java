b25dc9a391c7f822f9ae731ec4919f221004b3ed
gjrjr4545 <gjrjr4545@gmail.com>
2020-08-01 16:59:15 +0900

    12. 방문자 패턴

---
+++ src/com/person/design/visitor/Visitable.java
@@ -0,0 +1,5 @@
+package com.person.design.visitor;
+
+public interface Visitable {
+    public void accept(Visitor visitor);
+}
---
+++ src/com/person/design/visitor/Visitor.java
@@ -0,0 +1,6 @@
+package com.person.design.visitor;
+
+public interface Visitor {
+
+    public void visit(Visitable visitable);
+}
