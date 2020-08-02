eb88fa55d8330a737c2f9aa572b4215627807a57
gjrjr4545 <gjrjr4545@gmail.com>
2020-08-02 16:03:04 +0900

    14. 퍼사드 패턴

---
+++ src/com/person/design/facade/main/Application.java
@@ -0,0 +1,13 @@
+package com.person.design.facade.main;
+
+import com.person.design.facade.system.Facade;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.process();
+
+        Help
+    }
+}
